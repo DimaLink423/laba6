670da61d906d5dcff486d5257863e23aa254d834
linke <linke@DESKTOP-D9KFLQN>
2024-01-02 13:01:02 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+
+// proba 1
+namespace AES
+{
+
+    // class num 1
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AES aes = new AES(Encoding.UTF8.GetBytes("1234password1234"));
+
+        byte[] text = Encoding.UTF8.GetBytes("Some text here!!");
+        if (aes.Encrypt(text, ref text))
+            {
+                Console.WriteLine("Зашифрований текст = " + Encoding.UTF8.GetString(text));
+                aes.Decrypt(text, ref text);
+                Console.WriteLine("Розшифрований текст = " + Encoding.UTF8.GetString(text));
+            }
+            else
+                Console.WriteLine("Невірні дані!");
+
+
+
+
+
+        }
+    }
+}
